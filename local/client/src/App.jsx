2d171e4ddf79c59853f5af56dc5dde770cbb64bc
SrivaishnaviM15 <srivaim03@gmail.com>
2024-06-22 22:55:28 +0530

    libraryanlaysis base

--- local/client/src/App.jsx
+++ local/client/src/App.jsx
@@ -1,8 +1,8 @@
-import React, {useState} from "react";
+import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import StudentAnalysis from "./components/StudentAnalysis";
 // import Navbar from "./Navbar";
-// import LibraryAnalysis from "./components/LibraryAnalysis"
+import LibraryAnalysis from "./components/LibraryAnalysis"
 // import Home from "./Home"
 function App() {
 
@@ -13,7 +13,7 @@ function App() {
         {/* <Route path="/login" element={<LoginPage/>} />
         <Route path="/home" element={<HomePage />}/> */}
         <Route path="/studentanalysis" element={<StudentAnalysis/>}/>
-        {/* <Route path="/libraryanalysis" element={<LibraryAnalysis />}/> */}
+        <Route path="/libraryanalysis" element={<LibraryAnalysis />}/>
           </Routes>
 </Router>
   );
