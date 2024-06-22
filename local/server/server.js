5c9d2e851ae725f1b40729ef5171b2d568afde9f
Shivpreet <shivpreet16@gmail.com>
2024-06-22 18:02:17 +0530

    Setup

---
+++ local/server/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const cors=require('cors')
+const connectDB=require('./utils/connection')
+const app = express()
+require('dotenv').config();
+
+connectDB()
+
+app.use(cors())
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
